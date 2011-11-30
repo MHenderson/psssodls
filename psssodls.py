038d64d4de8bfbf18848cad97ad50b9bcdac07eb
Matthew Henderson <matthew.james.henderson@gmail.com>
2011-11-30 20:11:25 +0000

    Implemented latin constraints.

--- psssodls.py
+++ psssodls.py
@@ -3,9 +3,22 @@
 import sys
 
 def ell(p):
+  """This function takes a pair p = [p[0], p[1]] and returns the string
+  'l[p[0],p[1]]'."""
   return 'l[' + str(p[0]) + ',' + str(p[1]) + ']'
 
+def latin_constraints_str(n):
+  """Returns a string containing the latin constraints for a square of size
+  n."""
+  s = ''
+  for i in range(n):
+    s += 'alldiff(' + ell([i,'_']) + ')' + '\n'
+    s += 'alldiff(' + ell(['_',i]) + ')' + '\n'
+  return s
+
 def orthogonality_constraints_str(n):
+  """Returns a string containing the orthogonality constraints for a square
+  of size n."""
   def f(n):
     return [[(i,j), (j,i)] for i in range(n) for j in range(n)]
   def vecneq(p, q):
@@ -18,7 +31,10 @@ def orthogonality_constraints_str(n):
   return s
 
 def psssodls_string(n):
+  """Returns a string which is the entire Minion 3 format constraint program
+  for PSSSODLS(n)."""
   s = ''
+  s += latin_constraints_str(n)
   s += orthogonality_constraints_str(n)
   return s
 
