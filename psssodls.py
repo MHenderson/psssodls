83f7d976c450c47d608ff6178226601612fd7ff7
Matthew Henderson <matthew.james.henderson@gmail.com>
2011-12-01 10:20:08 +0000

    Adding some descriptive comments to the output.

--- psssodls.py
+++ psssodls.py
@@ -20,7 +20,7 @@ PRINT ALL
 
 **CONSTRAINTS**
 """ % (n, n, n - 1)
-  return s
+  return s + '\n'
 
 def end():
   return "**EOF**"
@@ -28,11 +28,11 @@ def end():
 def latin_constraints_str(n):
   """Returns a string containing the latin constraints for a square of size
   n."""
-  s = ''
+  s = '# Latin constraints. \n\n'
   for i in range(n):
     s += 'alldiff(' + ell([i,'_']) + ')' + '\n'
     s += 'alldiff(' + ell(['_',i]) + ')' + '\n'
-  return s
+  return s + '\n'
 
 def orthogonality_constraints_str(n):
   """Returns a string containing the orthogonality constraints for a square
@@ -41,12 +41,12 @@ def orthogonality_constraints_str(n):
     return [[(i,j), (j,i)] for i in range(n) for j in range(n)]
   def vecneq(p, q):
     return 'watchvecneq([' + ell(p[0]) + ',' + ell(p[1]) + '],[' +  ell(q[0]) + ',' + ell(q[1]) + '])'
-  s = ''
+  s = '# Orthogonality constraints. \n\n'
   for p in f(n):
     for q in f(n):
       if p > q:
         s += vecneq(p, q) + '\n'
-  return s
+  return s + '\n'
 
 def psssodls_string(n):
   """Returns a string which is the entire Minion 3 format constraint program
