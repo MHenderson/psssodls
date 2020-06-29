d59ee59fcc3040f567ebc055d1271e1963b74de1
Matthew Henderson <matthew.james.henderson@gmail.com>
2020-06-29 13:58:33 +0100

    now a package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='psssodls',
+      version='0.1',
+      description='Generate PSSSODLS in Minion 3 format.',
+      url='https://gitlab.com/MHenderson1/psssodls-generator',
+      author='Matthew Henderson',
+      author_email='matthew.james.henderson@gmail.com',
+      license='MIT',
+      packages=['psssodls'],
+      zip_safe=False)
