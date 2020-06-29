8359bf8a32cdde6db00aa6ce3e18bcbdbc6b297b
Matthew Henderson <matthew.james.henderson@gmail.com>
2020-06-29 14:43:49 +0100

    bump version to 0.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='psssodls',
-      version='0.1',
+      version='0.2',
       description='Generate PSSSODLS in Minion 3 format.',
       url='https://gitlab.com/MHenderson1/psssodls-generator',
       author='Matthew Henderson',
