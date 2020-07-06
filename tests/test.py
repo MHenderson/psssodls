0588ebc917a86d29ed790e9498376de4b265fa88
Matthew Henderson <matthew.james.henderson@gmail.com>
2020-07-06 11:15:04 +0100

    a test to isolate the failing pandiagonal constraints

--- tests/test.py
+++ tests/test.py
@@ -2,14 +2,15 @@
 Unit testing of the automatic batch processing application
 """
 import unittest
+import psssodls
  
-class AppTests(unittest.TestCase):
-    def test_app(self):
+class PandiagonalTests(unittest.TestCase):
+    def test_pandiagonal(self):
         """Simple Tests"""
-        self.assertEqual(1, 1)
-        self.assertNotEqual(2, 5)
+        p = psssodls.psumg(8, 3)
+        self.assertEqual(p, "sumgeq([l[0,3],l[1,4],l[2,5],l[3,6],l[4,7],l[5,0],l[6,1],l[7,2]],28)\n")
  
 def suite():
     _suite = unittest.TestSuite()
-    _suite.addTest(AppTests('test_app'))
+    _suite.addTest(PandiagonalTests('test_pandiagonal'))
     return _suite
