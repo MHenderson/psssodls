44f99b8af8398d5b52ffa79b4b3331456c655037
Matthew Henderson <matthew.james.henderson@gmail.com>
2024-05-07 08:44:47 +0000

    Import everything in main __init__.py.

--- src/psssodls/__init__.py
+++ src/psssodls/__init__.py
@@ -1,3 +1,4 @@
-# read version from installed package
-from importlib.metadata import version
-__version__ = version("psssodls")
+__version__ = "0.1.0"
+
+from .constraints import *
+from .main import *
